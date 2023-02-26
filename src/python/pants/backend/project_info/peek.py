0c958d29de1ef6bc491da78b5dca36a4edb47fce
Huon Wilson <huon@exoflare.io>
2023-02-26 20:20:11 +1100

    Add "transitive_fingerprints" if requested

--- src/python/pants/backend/project_info/peek.py
+++ src/python/pants/backend/project_info/peek.py
@@ -7,6 +7,9 @@ import collections
 import hashlib
 import json
 from dataclasses import asdict, dataclass, field, is_dataclass, replace
+
+# FIXME: only 3.9+
+from graphlib import TopologicalSorter
 from json import encoder
 from typing import Any, Iterable, Mapping
 
@@ -26,6 +29,8 @@ from pants.engine.target import (
     SourcesField,
     Target,
     Targets,
+    TransitiveTargets,
+    TransitiveTargetsRequest,
     UnexpandedTargets,
 )
 from pants.option.option_types import BoolOption
@@ -50,6 +55,11 @@ class PeekSubsystem(Outputting, GoalSubsystem):
         help="Whether to leave off values that match the target-defined default values.",
     )
 
+    include_transitive_fingerprints = BoolOption(
+        default=False,
+        help="Whether to include a `transitive_fingerprint` key.",
+    )
+
 
 class Peek(Goal):
     subsystem_cls = PeekSubsystem
@@ -70,14 +80,19 @@ class TargetData:
     expanded_dependencies: tuple[str, ...]
 
     intransitive_fingerprint: str = field(default="", init=False)
+    transitive_fingerprint: str | None = None
 
     def __post_init__(self) -> None:
-        d = self.to_dict(exclude_defaults=False, include_intransitive_fingerprint=False)
+        d = self.to_dict(
+            exclude_defaults=False, include_intransitive_and_transitive_fingerprints=False
+        )
         s = json.dumps(d, cls=_PeekJsonEncoder)
         super().__setattr__("intransitive_fingerprint", hashlib.sha256(s.encode()).hexdigest())
 
     def to_dict(
-        self, exclude_defaults: bool = False, include_intransitive_fingerprint: bool = True
+        self,
+        exclude_defaults: bool = False,
+        include_intransitive_and_transitive_fingerprints: bool = True,
     ) -> dict:
         nothing = object()
         fields = {
@@ -93,8 +108,11 @@ class TargetData:
             fields["sources"] = self.expanded_sources.files
             fields["sources_fingerprint"] = self.expanded_sources.digest.fingerprint
 
-        if include_intransitive_fingerprint:
+        if include_intransitive_and_transitive_fingerprints:
             fields["intransitive_fingerprint"] = self.intransitive_fingerprint
+
+            if self.transitive_fingerprint is not None:
+                fields["transitive_fingerprint"] = self.transitive_fingerprint
 
         return {
             "address": self.target.address.spec,
@@ -144,14 +162,21 @@ class _PeekJsonEncoder(json.JSONEncoder):
 async def get_target_data(
     # NB: We must preserve target generators, not replace with their generated targets.
     targets: UnexpandedTargets,
+    subsys: PeekSubsystem,
 ) -> TargetDatas:
     sorted_targets = sorted(targets, key=lambda tgt: tgt.address)
 
+    if subsys.include_transitive_fingerprints:
+        transitive_targets = await Get(
+            TransitiveTargets,
+            TransitiveTargetsRequest(roots=tuple(tgt.address for tgt in sorted_targets)),
+        )
+        targets_to_analyze = transitive_targets.closure
+    else:
+        targets_to_analyze = sorted_targets
+
     # We "hydrate" sources fields with the engine, but not every target has them registered.
-    targets_with_sources = []
-    for tgt in sorted_targets:
-        if tgt.has_field(SourcesField):
-            targets_with_sources.append(tgt)
+    targets_with_sources = [tgt for tgt in targets_to_analyze if tgt.has_field(SourcesField)]
 
     # NB: When determining dependencies, we replace target generators with their generated targets.
     dependencies_per_target = await MultiGet(
@@ -159,30 +184,51 @@ async def get_target_data(
             Targets,
             DependenciesRequest(tgt.get(Dependencies), include_special_cased_deps=True),
         )
-        for tgt in sorted_targets
+        for tgt in targets_to_analyze
     )
     hydrated_sources_per_target = await MultiGet(
         Get(HydratedSources, HydrateSourcesRequest(tgt[SourcesField]))
         for tgt in targets_with_sources
     )
 
-    expanded_dependencies = [
-        tuple(dep.address.spec for dep in deps)
-        for tgt, deps in zip(sorted_targets, dependencies_per_target)
-    ]
+    expanded_dependencies = {
+        tgt.address: tuple(dep.address for dep in deps)
+        for tgt, deps in zip(targets_to_analyze, dependencies_per_target)
+    }
     expanded_sources_map = {
         tgt.address: hs.snapshot
         for tgt, hs in zip(targets_with_sources, hydrated_sources_per_target)
     }
 
-    return TargetDatas(
-        TargetData(
+    data = {
+        tgt.address.spec: TargetData(
             tgt,
-            expanded_dependencies=expanded_deps,
+            expanded_dependencies=tuple(a.spec for a in expanded_dependencies.get(tgt.address, ())),
             expanded_sources=expanded_sources_map.get(tgt.address),
         )
-        for tgt, expanded_deps in zip(sorted_targets, expanded_dependencies)
-    )
+        for tgt in targets_to_analyze
+    }
+
+    if subsys.include_transitive_fingerprints:
+        sorter = TopologicalSorter()
+        for address, deps in expanded_dependencies.items():
+            sorter.add(address, *deps)
+
+        for address in sorter.static_order():
+            deps = [
+                (dep, data[dep].transitive_fingerprint)
+                for dep in sorted(data[address.spec].expanded_dependencies)
+            ]
+            this = data[address.spec]
+            serialized = json.dumps([this.intransitive_fingerprint, deps])
+            data[address.spec] = replace(
+                this, transitive_fingerprint=hashlib.sha256(serialized.encode()).hexdigest()
+            )
+
+    # TODO: cycles occur in practice; can be handled by instead processing strongly connected
+    # components
+
+    return TargetDatas(data[tgt.address.spec] for tgt in sorted_targets)
 
 
 @goal_rule
